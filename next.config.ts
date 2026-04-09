52feb6d736c7a41bbaa7013903bd6402061ebe91
Ahmet <ahmet@mirros.app>
2026-04-09 14:10:36 +0300

    deploy: force standalone mode and add deployment marker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_DEPLOY_TIME: "1775733031123",
+  }
 };
 
 export default nextConfig;
